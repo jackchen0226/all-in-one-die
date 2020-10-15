353991870fdcb4e4419c133c891ff49093781b74
repl.it user <replituser@example.com>
2020-10-15 02:35:32 +0000

    Created base code base

--- src/main.c
+++ src/main.c
@@ -1,7 +1,57 @@
-void setup(){
-  Serial.begin(9600);
+float X, Y, Z;
+
+float roll_threshold = 5;
+
+int maxRoll = 20;
+ 
+void setup() {
+  //Serial.begin(9600);
 }
 
-void loop(){
+void loop() {
+
+  if (getTotalAcceleration() > roll_threshold) {
+    rollDice();
+}
+
+float getTotalAcceleration(){
+    // Compute total acceleration
+  X = 0;
+  Y = 0;
+  Z = 0;
+  for (int i=0; i<10; i++) {
+    X += CircuitPlayground.motionX();
+    Y += CircuitPlayground.motionY();
+    Z += CircuitPlayground.motionZ();
+    delay(1);
+  }
+  X /= 10;
+  Y /= 10;
+  Z /= 10;
+ 
+  return sqrt(X*X + Y*Y + Z*Z);
+}
+
+void updateDisplay(int num){
   
+}
+
+void rollDice(){
+  int newRoll = Random.Range(0, maxRoll);
+
+  updateDisplay(newRoll);
+}
+
+void increaseMaxRoll(){
+  maxRoll++;
+  if(maxRoll > 20){
+    maxRoll = 20;
+  }
+}
+
+void decreaseMaxRoll(){
+  maxRoll--;
+  if(maxRoll < 4){
+    maxRoll = 4;
+  }
 }
